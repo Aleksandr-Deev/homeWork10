9e46e544da096a79941f6872606701d09d9af7e8
Aleksandr Deev <geshtait@mail.ru>
2022-09-20 22:22:38 +0500

    Сделал ДЗ 1.7 Строки

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
+import java.util.Locale;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+        // Домашнее задание 1.7 Стоки
+        // Задание 1
+
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println(fullName);
+
+        // Задание 2
+        System.out.println();
+        String fullNameUp = fullName.toUpperCase(Locale.ROOT);
+        System.out.println("Данные ФИО сотрудника для заполнения отчета - " + fullNameUp);
+
+        // Задание 3
+        System.out.println();
+        fullName = "Иванов Семён Семёнович";
+        String newFullName = fullName.replace("ё", "е");
+        System.out.println("Данные ФИО сотрудника - " + newFullName);
+
     }
 }
